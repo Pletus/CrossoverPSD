6feb97d524aed7872a82a46a3b6a69ba5888d77b
YagoPazos <yagopb@gmail.com>
2024-06-10 13:47:38 +0200

    added hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import React from "react";
+import Happy from './components/Happy'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='p-4 md:p-6 lg:p-10'>
+      <Happy />
+    </div>
   )
 }
 
