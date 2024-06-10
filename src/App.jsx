0e7bdcb6a0974bd47729fe0240f85bd5f67d41d3
Anitha <anithamca.k2008@gmail.com>
2024-06-10 15:14:22 +0200

    merge head & footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import React from "react";
 import Happy from './components/Happy'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='p-4 md:p-6 lg:p-10'>
       <Happy />
+     <Footer />
     </div>
   )
 }
